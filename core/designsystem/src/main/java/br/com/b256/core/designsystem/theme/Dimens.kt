e57be840ba7ad861cd26a355bf4a39ece932d6e8
eugenio.cunha <eugenio.cunha@ymail.com>
2024-11-26 19:11:45 -0300

    feat: implemento uma lista de dimensões

---
+++ core/designsystem/src/main/java/br/com/b256/core/designsystem/theme/Dimens.kt
@@ -0,0 +1,39 @@
+package br.com.b256.core.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+
+val BorderHalf = 0.5.dp
+val BorderSingle = 2.dp
+val BorderDouble = 4.dp
+val BorderTreble = 8.dp
+val BorderQuad = 16.dp
+
+val StrokeHalf = 1.dp
+val StrokeSingle = 2.dp
+val StrokeDouble = 4.dp
+val StrokeTreble = 8.dp
+val StrokeQuad = 16.dp
+
+val PaddingHalf = 4.dp
+val PaddingSingle = 8.dp
+val PaddingDouble = 16.dp
+val PaddingTreble = 24.dp
+val PaddingQuad = 32.dp
+
+val SpacerHalf = 4.dp
+val SpacerSingle = 8.dp
+val SpacerDouble = 16.dp
+val SpacerTreble = 24.dp
+val SpacerQuad = 32.dp
+
+val ElevationHalf = 4.dp
+val ElevationSingle = 8.dp
+val ElevationDouble = 16.dp
+val ElevationTreble = 24.dp
+val ElevationQuad = 32.dp
+
+val IconHalf = 12.dp
+val IconSingle = 18.dp
+val IconDouble = 24.dp
+val IconTreble = 36.dp
+val IconQuad = 48.dp
