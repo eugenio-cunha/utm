ff7c7cf0f5c03fe0110ec3d4506d0ce3e9b9c978
eugenio.cunha <eugenio.cunha@ymail.com>
2024-12-02 20:07:35 -0300

    feat: adiciono novos icones

--- core/designsystem/src/main/java/br/com/b256/core/designsystem/icon/B256Icons.kt
+++ core/designsystem/src/main/java/br/com/b256/core/designsystem/icon/B256Icons.kt
@@ -2,12 +2,26 @@ package br.com.b256.core.designsystem.icon
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.automirrored.outlined.DirectionsWalk
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Autorenew
+import androidx.compose.material.icons.outlined.Bookmarks
 import androidx.compose.material.icons.outlined.Camera
 import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.DirectionsWalk
 import androidx.compose.material.icons.outlined.Done
+import androidx.compose.material.icons.outlined.GpsFixed
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Map
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material.icons.outlined.Place
+import androidx.compose.material.icons.outlined.Save
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material.icons.outlined.SubdirectoryArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.ui.graphics.vector.ImageVector
 
@@ -16,11 +30,21 @@ import androidx.compose.ui.graphics.vector.ImageVector
  * B256. Ícones de material são [ImageVector]s, ícones personalizados são IDs de recursos desenháveis.
  * */
 object B256Icons {
-    val ArrowBack = Icons.AutoMirrored.Outlined.ArrowBack
-    val More = Icons.Outlined.MoreVert
-    val Camera = Icons.Outlined.Camera
-    val Dismiss = Icons.Outlined.Clear
     val Accept = Icons.Outlined.Done
-    val Place = Icons.Outlined.Place
+    val ArrowBack = Icons.AutoMirrored.Outlined.ArrowBack
+    val Camera = Icons.Outlined.Camera
+    val Close = Icons.Outlined.Close
+    val Dismiss = Icons.Outlined.Clear
     val Home = Icons.Outlined.Home
+    val More = Icons.Outlined.MoreVert
+    val Place = Icons.Filled.Place
+    val Search = Icons.Outlined.Search
+    val Subdirectory = Icons.Outlined.SubdirectoryArrowRight
+    val Settings = Icons.Outlined.Settings
+    val Map = Icons.Outlined.Map
+    val Gps = Icons.Outlined.GpsFixed
+    val Walk = Icons.AutoMirrored.Outlined.DirectionsWalk
+    val Share = Icons.Outlined.Share
+    val Add = Icons.Outlined.Add
+    val Auto = Icons.Outlined.Autorenew
 }
