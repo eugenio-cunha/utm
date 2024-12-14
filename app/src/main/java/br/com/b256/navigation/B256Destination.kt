43cd5c026dd77392bc8db14fbcaaa638832beab6
eugenio.cunha <eugenio.cunha@ymail.com>
2024-12-14 13:32:40 -0300

    refactor: remove propriedades não utilizadas

--- app/src/main/java/br/com/b256/navigation/B256Destination.kt
+++ app/src/main/java/br/com/b256/navigation/B256Destination.kt
@@ -1,20 +1,12 @@
 package br.com.b256.navigation
 
-import androidx.annotation.StringRes
-import androidx.compose.ui.graphics.vector.ImageVector
-import br.com.b256.R
-import br.com.b256.core.designsystem.icon.B256Icons
 import br.com.b256.feature.utm.navigation.UtmRoute
 import kotlin.reflect.KClass
 
 enum class B256Destination(
-    val icon: ImageVector,
-    @StringRes val titleTextId: Int,
     val route: KClass<*>,
 ) {
     UTM(
-        icon = B256Icons.Place,
-        titleTextId = R.string.app_place,
         route = UtmRoute::class,
     ),
 }
